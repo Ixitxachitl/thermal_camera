4464b8871214a3d3f0a81626ec4394e37dea3a81
Noah <kramerfm@gmail.com>
2026-02-14 09:37:49 -0800

    Remove CONNECTION_CLASS from ThermalCameraConfigFlow

--- custom_components/thermal_camera/config_flow.py
+++ custom_components/thermal_camera/config_flow.py
@@ -31,7 +31,6 @@ class ThermalCameraConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for the Thermal Camera integration."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
